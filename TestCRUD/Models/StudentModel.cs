135a0aa1d2c72407f457552aa2109c4b740eed19
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-09 21:56:08 +1200

    added student model

---
+++ TestCRUD/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TestCRUD.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string University { get; set; }
+        public string City { get; set; }
+    }
+}
