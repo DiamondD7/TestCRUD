113f1902f57f536ab9508800766dc1c233ad5c5b
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-09 22:14:12 +1200

    added db context

--- TestCRUD/Data/ApplicationDbContext.cs
+++ TestCRUD/Data/ApplicationDbContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestCRUD.Models;
 
 namespace TestCRUD.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<StudentModel> Students { get; set; }
     }
 }
